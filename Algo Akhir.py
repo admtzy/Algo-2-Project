16d4993cb200414ba8c4a5763c648bca2abbfeda
iZall. <radityafd07@gmail.com>
2025-06-03 22:29:42 +0700

    baru (#3)

--- Algo Akhir.py
+++ Algo Akhir.py
@@ -1,6 +1,10 @@
 import webbrowser
 import os
 
+<<<<<<< HEAD
+# file_path = os.path.abspath("peta.html")
+# webbrowser.open(f"file://{file_path}")
+=======
 file_path = os.path.abspath("peta.html")
 webbrowser.open(f"file://{file_path}")
 
@@ -8,3 +12,4 @@ webbrowser.open(f"file://{file_path}")
 # haloo \
 
 # mhaloo
+>>>>>>> a4790893ec1dfc8fac1af538d5715bd5bae14f0d
