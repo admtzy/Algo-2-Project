70aed3be7695a754fd2bb0f538d0d1a6839f3190
Syadidch <syadidchoiri@gmail.com>
2025-06-03 23:30:44 +0700

    adad

--- Algo Akhir.py
+++ Algo Akhir.py
@@ -1,10 +1,31 @@
+import psycopg2
 import webbrowser
 import os
 
-file_path = os.path.abspath("peta.html")
-webbrowser.open(f"file://{file_path}")
+# file_path = os.path.abspath("peta.html")
+# webbrowser.open(f"file://{file_path}")
 
-# masuk gak guys? hah
-# haloo \
+def connectdb():
+    conn = psycopg2.connect(
+    host="localhost",
+    database="Algo2",
+    user="postgres",
+    password="syadid1306",
+    port=5432
+    )
+    return conn
 
-# mhaloo
+conn=connectdb()
+cur = conn.cursor()
+
+# Eksekusi query SQL
+cur.execute("SELECT * FROM akun;")
+
+# Ambil hasil
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+# Tutup koneksi
+cur.close()
+conn.close()
---
+++ Did.py
@@ -0,0 +1,7 @@
+import webbrowser
+import os
+
+file_path = os.path.abspath("peta.html")
+webbrowser.open(f"file://{file_path}")
+
+input()
